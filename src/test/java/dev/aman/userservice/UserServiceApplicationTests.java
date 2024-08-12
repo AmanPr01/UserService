18c24f374edaf89ba0b8167f23dc6df583a3487e
AmanPr01 <me.aman.pr@gmail.com>
2024-08-12 21:05:04 +0530

    Some addition to test

--- src/test/java/dev/aman/userservice/UserServiceApplicationTests.java
+++ src/test/java/dev/aman/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,48 @@
 package dev.aman.userservice;
 
+import dev.aman.userservice.Security.Repositories.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+
+	@Autowired
+	private JpaRegisteredClientRepository registeredClientRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void addRegisteredClientToDB() {
+		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("oidc-client")
+				.clientSecret("{noop}secret")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.redirectUri("https://oauth.pstmn.io/v1/callback")
+				.postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+				.scope(OidcScopes.OPENID)
+				.scope(OidcScopes.PROFILE)
+				.scope("ADMIN")
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+
+		registeredClientRepository.save(registeredClient);
+	}
+	/*
+	This above code comes from security config class.
+	It is commented out there.
+	We just want an entry in the database.
+	 */
 }
