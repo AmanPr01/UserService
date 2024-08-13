160f77cefbffecc40f086aad82c371ed3a7e55eb
AmanPr01 <me.aman.pr@gmail.com>
2024-08-13 16:08:05 +0530

    Kafka

--- src/main/java/dev/aman/userservice/Services/UserServiceImpl.java
+++ src/main/java/dev/aman/userservice/Services/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package dev.aman.userservice.Services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.aman.userservice.DTOs.SendEmailEventDTO;
 import dev.aman.userservice.Models.Token;
 import dev.aman.userservice.Models.User;
 import dev.aman.userservice.Repositories.TokenRepository;
 import dev.aman.userservice.Repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,14 +24,21 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     protected BCryptPasswordEncoder bCryptPasswordEncoder;
     private TokenRepository tokenRepository;
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private ObjectMapper objectMapper;
+    // By default, when we install Spring kafka, since it is coming from spring framework, it automatically gives the bean of that.
+    // Object mapper comes from jackson, it converts java object to JSON.
 
     public UserServiceImpl(UserRepository userRepository,
                            BCryptPasswordEncoder bCryptPasswordEncoder,
-                           TokenRepository tokenRepository) {
+                           TokenRepository tokenRepository,
+                           KafkaTemplate kafkaTemplate,
+                           ObjectMapper objectMapper) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.tokenRepository = tokenRepository;
-
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -46,6 +57,22 @@ public class UserServiceImpl implements UserService {
             user.setHashedPassword(bCryptPasswordEncoder.encode(password));
 
             user = userRepository.save(user);
+
+            // Publish the user inside the Queue.
+            SendEmailEventDTO emailEventDTO = new SendEmailEventDTO();
+            emailEventDTO.setTo(email);
+            emailEventDTO.setFrom("felixfether@gmail.com");
+            emailEventDTO.setSubject("Welcome to Scaler");
+            emailEventDTO.setBody("Welcome to Scaler. We are very happy to have you on our platform. All the best.");
+
+            try {
+                kafkaTemplate.send(
+                        "sendEmail",
+                        objectMapper.writeValueAsString(emailEventDTO)
+                );
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         return user;
